7fba0883dcf894326a51e894b5e1f2c8d3e3fa9b
Alexandre Lindele <alexandre.lindele@outlook.de>
2024-02-29 16:21:17 +0100

    Updated PageObjects methods for Selenium Testing

--- src/test/java/com/udacity/jwdnd/course1/cloudstorage/RegistrationAndLoginTests.java
+++ src/test/java/com/udacity/jwdnd/course1/cloudstorage/RegistrationAndLoginTests.java
@@ -1,6 +1,6 @@
 package com.udacity.jwdnd.course1.cloudstorage;
 
-
+import java.time.Duration;
 import com.udacity.jwdnd.course1.cloudstorage.PageObjects.LoginPageObject;
 import com.udacity.jwdnd.course1.cloudstorage.PageObjects.SignupPageObject;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
@@ -9,8 +9,15 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RegistrationAndLoginTests {
@@ -33,12 +40,24 @@ public class RegistrationAndLoginTests {
     void userRegistrationTest(){
         LoginPageObject loginPage = new LoginPageObject(this.webDriver);
         loginPage.clickRegistrationLink();
-
         SignupPageObject signupPage = new SignupPageObject(this.webDriver);
+        signupPage.inputFirstname("Alex");
+        signupPage.inputLastName("Lindele");
+        signupPage.inputUsername("Alex");
+        signupPage.inputPassword("password");
+        assertNotNull(signupPage.getSuccessMsg());
+        assertEquals(null,signupPage.getErrorMsg());
+    }
 
-        signupPage.
-
-
+    @Test
+    void userLoginTest(){
+        LoginPageObject loginPage = new LoginPageObject(this.webDriver);
+        loginPage.inputUserName("Alex");
+        loginPage.inputPassword("password");
+        loginPage.clickLoginButton();
+        //TODO Complete test with checking HomepageElements
+        WebDriverWait webDriverWait = new WebDriverWait(webDriver, Duration.ofMillis(2000));
+        webDriverWait.until(ExpectedConditions.titleIs("Home"));
     }
 
 }
