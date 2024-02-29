404792d7eab705605b25aead1fff3cd1fb0d1e7f
Alexandre Lindele <alexandre.lindele@outlook.de>
2024-02-29 19:24:38 +0100

    Added UpdateFunction to UserMapper

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/UserMapper.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/UserMapper.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.*;
 @Mapper
 public interface UserMapper {
 
-
+    //CREATE
     @Insert("INSERT INTO USERS (username, salt, password, firstname, lastname)"+
     "VALUES(#{username}, #{salt}, #{password}, #{firstname}, #{lastname})")
     @Options(useGeneratedKeys = true, keyProperty = "userid")
@@ -16,12 +16,18 @@ public interface UserMapper {
 
 
     //TODO Check if User or Integer Userid should be used
-
+    //DELETE
     @Delete("DELETE * FROM USERS WHERE username = #{username}")
     void deleteUser(String username);
 
+    //READ
     @Select("SELECT * FROM USERS WHERE username = #{username}")
     UserModel getUser(String username);
+    //UPDATE
+    @Update("UPDATE USERS"+
+    "SET username = #{username}, salt = #{salt}, password=#{password}, firstname = #{firstname}, lastname = #{lastname}"+
+            "WHERE userid = #{userid}")
+    void updateUser(UserModel user);
 
 
 
