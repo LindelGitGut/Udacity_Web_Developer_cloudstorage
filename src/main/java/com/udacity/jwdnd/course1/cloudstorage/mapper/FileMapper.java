2028e5726a7e21d65f9866791f7f9ccacbd3b98a
Alexandre Lindele <alexandre.lindele@outlook.de>
2024-03-02 11:32:07 +0100

    Added Mapper CRUD Methods for Files

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/FileMapper.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/FileMapper.java
@@ -1,9 +1,29 @@
 package com.udacity.jwdnd.course1.cloudstorage.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
+import com.udacity.jwdnd.course1.cloudstorage.models.FileModel;
+import org.apache.ibatis.annotations.*;
+
+import java.io.File;
 
 @Mapper
 public interface FileMapper {
     //TODO Implement Mapper
+
+    //CREATE
+    @Insert("INSERT INTO FILES (filename, contenttype, filesize, userid, BLOB)"+
+    "VALUES(#{filename}, #{contenttype}, #{filesize}, #{userid}, #{BLOB})")
+    Integer createFile(FileModel file);
+
+    //READ
+    @Select("SELECT * FROM FILES WHERE fileId = #{fileId}")
+    FileModel getFile(Integer fileId);
+
+    //UPDATE
+    @Update("UPDATE FILES SET (fileId = #{fileId}, filename = #{filename}, conetenttype = #{contenttype}, filesize = #{filesize}, userid = #{userid}, BLOB = #{BLOB})")
+    Integer updateFile(FileModel file);
+
+    //REMOVE
+    @Delete("DELETE * FROM FILES WHERE fileId = #{fileId}")
+    Integer removeFile(FileModel file);
 
 }
