2e5d4237cf95290704b7a203b9bb351794fc0591
Alexandre Lindele <alexandre.lindele@outlook.de>
2024-03-04 16:55:48 +0100

    sperated download requestmapping from home requestmapping

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -4,6 +4,10 @@ package com.udacity.jwdnd.course1.cloudstorage.controller;
 import com.udacity.jwdnd.course1.cloudstorage.models.FileModel;
 import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
 import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +19,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @Controller
-@RequestMapping("/home")
+
 public class HomeController {
 
     UserService userService;
@@ -28,6 +32,7 @@ public class HomeController {
 
     //handling File Upload
     @PostMapping
+    @RequestMapping("/home")
     String fileUpload(@RequestParam("fileUpload") MultipartFile file, Model model) throws IOException {
 
         if (file != null) {
@@ -58,32 +63,47 @@ public class HomeController {
 
 
     @GetMapping
-    String getHomepage(Model model,
-
-                       @RequestParam(value = "deletefile",required = false) String deleteFileID,
-                       @RequestParam(value = "downloadfile",required = false) String downloadFileID){
+    @RequestMapping("/home")
+    String getHomepage(Model model, @RequestParam(value = "deletefile", required = false) String deleteFileID) {
 
 
+        if (deleteFileID != null) {
+            //TODO Add cheack if File was Successfull or unsucessfull deleted, Foreward to Result Page
 
-        if (deleteFileID != null){
             System.out.println("Debug deleteFile Controller getriggert");
             fileService.removeFile(deleteFileID);
             model.addAttribute("allFiles", fileService.getCurretUserFiles());
         }
-        if (downloadFileID != null){
-            System.out.println("Debug downloadfile getriggert");
-            FileModel downloadfile = fileService.getFile(downloadFileID);
-            if(downloadfile == null){
-                System.out.println("Debug Downloadfile, File konnte nicht gefunden werden");
-            }
 
-            else {
-                System.out.println("Debug Download File, File gefunden");
-            }
-        }
 
         model.addAttribute("allFiles", fileService.getCurretUserFiles());
         return "home";
     }
 
+    @GetMapping
+    @RequestMapping("/home/download")
+    ResponseEntity getDownload(@RequestParam(value = "downloadfile", required = false) String downloadFileID) {
+
+        if (downloadFileID != null) {
+
+            System.out.println("Debug downloadfile getriggert");
+            FileModel downloadfile = fileService.getFile(downloadFileID);
+            if (downloadfile == null) {
+                System.out.println("Debug Downloadfile, File konnte nicht gefunden werden");
+                return ResponseEntity.notFound().build();
+            } else {
+                System.out.println("Debug Download File, File gefunden");
+                return ResponseEntity.ok()
+                        .contentType(MediaType.parseMediaType(downloadfile.getContenttype()))
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + downloadfile.getFilename() + "\"")
+                        .body(new ByteArrayResource(downloadfile.getFiledata()));
+            }
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
 }
+
+
+
