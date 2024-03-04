4497a6fa60999b06e70da80a8949dfdc570a636f
Alexandre Lindele <alexandre.lindele@outlook.de>
2024-03-04 14:43:10 +0100

    addec Controller for Fileupload

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -1,13 +1,52 @@
 package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 
+import com.udacity.jwdnd.course1.cloudstorage.models.FileModel;
+import com.udacity.jwdnd.course1.cloudstorage.services.FileService;
+import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
+
+    UserService userService;
+    FileService fileService;
+
+    public HomeController(UserService userService, FileService fileService) {
+        this.userService = userService;
+        this.fileService = fileService;
+    }
+
+    //handling File Upload
+    @PostMapping
+    String fileUpload(@RequestParam("fileUpload") MultipartFile file, Model model) throws IOException {
+        String filename = file.getOriginalFilename();
+        String contentype = file.getContentType();
+        String filesize = "" + file.getSize();
+        Integer userid = userService.getCurrentUser().getUserid();
+        byte[] BLOB = file.getBytes();
+
+        fileService.createFile(new FileModel(null,filename,contentype,filesize,userid,BLOB));
+
+
+        model.addAttribute("allFiles", fileService.getCurretUserFiles());
+
+        System.out.println("Debug HomeController Fileupload" +
+                " Filename: " + filename +" contenttype: " + contentype+ " filesize:" + filesize+ " userid:" +userid);
+
+
+
+        return "home";
+    }
 
 
     @GetMapping
