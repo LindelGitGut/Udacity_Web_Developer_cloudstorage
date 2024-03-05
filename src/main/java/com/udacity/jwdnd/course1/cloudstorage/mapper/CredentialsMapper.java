ba2bc5784b22f849a8bc818596f1ed111c67588b
Alexandre Lindele <alexandre.lindele@outlook.de>
2024-03-05 12:11:36 +0100

    created Mapper for Credentials

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/CredentialsMapper.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/CredentialsMapper.java
@@ -4,23 +4,41 @@ package com.udacity.jwdnd.course1.cloudstorage.mapper;
 import com.udacity.jwdnd.course1.cloudstorage.models.CredentialModel;
 import org.apache.ibatis.annotations.*;
 
+import java.util.List;
+
 @Mapper
 public interface CredentialsMapper {
 
+/*
+    Integer credentialid;
+    String url;
+    String username;
+    String key;
+    String password;
+    Integer userid;
+
+    */
+
+
+
     // Create
-    @Insert("INSERT INTO CREDENTIALS() VALUES()")
-    @Options(useGeneratedKeys = true)
+    @Insert("INSERT INTO CREDENTIALS(url,username,key,password,userid) VALUES(#{url},#{username},#{key},#{password},#{userid})")
+    @Options(useGeneratedKeys = true, keyProperty = "credentialid")
     Integer createCredentials(CredentialModel credentialModel);
 
 
     //Read
-    //@Select()
+    @Select("SELECT * FROM CREDENTIALS where userid = #{userid} AND credentialid = #{credentialid}")
+    CredentialModel getCredential(String credentialid, String userid);
 
+    @Select("SELECT * FROM CREDENTIALS where userid = #{userid}")
+    List<CredentialModel> getallUserCredentials(String credentialid, String userid);
     //UPDATE
-    //@Update()
-
+    @Update("UPDATE CREDENTIALS SET credentialid = #{credentialid} ,url=#{url},username = #{username},key = #{key},password = #{password},userid = #{userid}" +
+            "WHERE userid = #{userid} AND credentialid = #{credentialid}")
+    Integer changeCredential(CredentialModel credentialModel);
     //DELETE
-
-    //@Delete("DELETE FROM CREDENTIALS WHERE")
+    @Delete("DELETE FROM CREDENTIALS WHERE userid = #{userid} AND credentialid = #{credentialid}")
+    Integer deleteCredential(String userid, String credetialid);
 
 }
